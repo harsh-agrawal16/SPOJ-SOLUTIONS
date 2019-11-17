12a531459a7ca1f479d649dce5d7b0f988bf79eb
Harsh Agrawal <40310961+harsh-agrawal16@users.noreply.github.com>
2019-11-17 10:59:20 +0530

    Create ELEVTRLBL.cpp

---
+++ ELEVTRLBL.cpp
@@ -0,0 +1,52 @@
+/*Author :- Harsh Agrawal
+  Question:- SPOJ ELEVTRBL
+  */
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+  ll f,s,g,u,d,curr;
+  cin>>f>>s>>g>>u>>d;
+
+  bool found = false;
+  ll clicks[f+1]; //to keep count of the no of button presses to reach the respective floor
+  bool visited[f+1]; // to keep track of visited nodes
+  memset(visited,false,sizeof(visited));
+  queue<ll> q;
+
+  clicks[s]=0;
+  q.push(s);
+  visited[s]=true;
+  while(!q.empty()){
+    curr = q.front();
+    q.pop();
+
+    if(curr==g){
+      found = true;
+      break;
+    }
+
+    if(curr+u<=f && !visited[curr+u]){
+      visited[curr+u] = true;
+      clicks[curr+u] = clicks[curr]+1;
+      q.push(curr+u);
+    }
+
+    if(curr-d>=1 && !visited[curr-d]){
+      visited[curr-d] = true;
+      clicks[curr-d] = clicks[curr]+1;
+      q.push(curr-d);
+    }
+  }
+
+  if(found){
+    cout<<clicks[g]<<endl;
+  }
+  else{
+  	cout<<"use the stairs"<<endl;
+  }
+
+}
